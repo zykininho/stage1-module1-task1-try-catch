cc92f6475d6456846c754c8b204cae82298b4551
MZykin <zykininho@gmail.com>
2023-08-06 23:44:36 +0600

    feat: added function to parse int and sum it

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,8 +21,13 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                int number = Integer.parseInt(next);
+                sum += number;
+            } catch (NumberFormatException e) {
+                justWords += " " + next;
+            }
+
         }
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
